0097ed2bf98acf88f2e65b9d95976efdb5efe334
nathan hensel <nathanihensel@gmail.com>
2023-02-05 00:45:54 -0600

    enforce elasticsearch uid 1000 ownership in volume

--- main.tf
+++ main.tf
@@ -48,6 +48,20 @@ resource "kubernetes_deployment" "main" {
         }
       }
       spec {
+        init_container {
+          image = "alpine"
+          name  = "chown-elasticsearch"
+          volume_mount {
+            name       = "${var.stack}-${var.context}"
+            mount_path = "/usr/share/elasticsearch/data"
+          }
+          command = [
+            "chown",
+            "-R",
+            "1000:0",
+            "/usr/share/elasticsearch/data"
+          ]
+        }
         container {
           image = var.image
           name  = "${var.stack}-${var.context}"
